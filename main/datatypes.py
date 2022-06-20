fdcb9820f17bb3f9aae11a275949a295910094c9
Oliver Gregorius <oliver@gregorius.dev>
2022-06-20 23:37:51 +0200

    Added first code

---
+++ main/datatypes.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class DataType(Enum):
+    STRING = "string"
+    UINT16_BE = "uint16"
+    UINT32_BE = "uint32"
+    INT16_BE = "int16"
+    INT32_BE = "int32"
+    BITFIELD16 = "bitfield16"
+    BITFIELD32 = "bitfield32"
+    MULTIDATA = "multidata"
+
+
+def decode_string(value):
+    return value.decode("utf-8", "replace").strip("\0")
+
+
+def decode_uint_be(value):
+    return int.from_bytes(value, byteorder="big", signed=False)
+
+
+def decode_int_be(value):
+    return int.from_bytes(value, byteorder="big", signed=True)
+
+
+def decode_bitfield(value):
+    return ''.join(format(byte, '08b') for byte in value)
+
+
+def decode(value, type):
+    match type:
+        case DataType.STRING:
+            return decode_string(value)
+        case DataType.UINT16_BE | DataType.UINT32_BE:
+            return decode_uint_be(value)
+        case DataType.INT16_BE | DataType.INT32_BE:
+            return decode_int_be(value)
+        case DataType.BITFIELD16 | DataType.BITFIELD32:
+            return decode_bitfield(value)
+        case DataType.MULTIDATA:
+            return value
+        case _:
+            raise ValueError("Unknown register type")
---
+++ main/sun2000_modbus.py
@@ -0,0 +1,55 @@
+import logging
+import time
+
+from pymodbus.client.sync import ModbusTcpClient
+
+from main.datatypes import decode
+from main.registers import MeterEquipmentRegister
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Sun2000:
+
+    def __init__(self, host, port=502, timeout=5, wait=2, slave=0):
+        self.wait = wait
+        self.connected = False
+        self.inverter = ModbusTcpClient(host, port, timeout=timeout, unit_id=slave)
+
+    def connect(self):
+        if not self.connected:
+            self.connected = self.inverter.connect()
+            time.sleep(self.wait)
+            if self.connected:
+                logging.info("Successfully connected to inverter")
+            else:
+                logging.error("Connection to inverter failed")
+
+    def read_raw_value(self, register):
+        register_value = self.inverter.read_holding_registers(register.value.address, register.value.quantity)
+
+        return decode(register_value.encode()[1:], register.value.type)
+
+    def read(self, register):
+        raw_value = self.read_raw_value(register)
+
+        if register.value.gain == 1:
+            return raw_value
+        else:
+            return raw_value / register.value.gain
+
+    def read_formatted(self, register):
+        value = self.read(register)
+
+        if register.value.unit is not None:
+            return f'{value} {register.value.unit}'
+        else:
+            return value
+
+
+#### Testing
+inv = Sun2000("192.168.200.1", 6607, slave=1)
+inv.connect()
+for register in MeterEquipmentRegister:
+    print(f'{register.name}: {inv.read_formatted(register)}')
+# print(f'MaximumReactivePowerAbsorbedFromTheGrid: {inv.read_formatted(InverterEquipmentRegister.State1)}')
